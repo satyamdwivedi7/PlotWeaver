454d7b672130b3519c920b66bbe1e108106e37a1
Raghvendra Yadav <raghvendrayadav98400@gmail.com>
2024-09-21 14:33:58 +0530

    added chapters

---
+++ src/components/Chapter.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Chapters = () => {
+  return (
+    <div className=' flex justify-around items-center w-[90%] mx-auto py-[2rem]'>
+        <div className="flex flex-col gap-[1rem]">
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-gray-500 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-blue-400 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-gray-500 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-blue-400 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-gray-500 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+        </div>
+        <div className="flex flex-col gap-[1rem]">
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-blue-400 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-gray-500 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-blue-400 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-gray-500 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+            <div className="flex flex-row justify-between pl-[1.7rem] pr-[0.6rem] rounded-lg border border-white-500 w-[30rem] h-[3.5rem] bg-blue-400 items-center pt-2">
+                <p className='text-[2rem] text-white'>Chapter 1</p>
+                
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Chapters
